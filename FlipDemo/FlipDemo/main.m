beb72d18fe7c5449d74430af1ebb1b6a7072e806
WangKJ <wangjie_app@163.com>
2016-06-01 17:40:28 +0800

    一个动画的demo

---
+++ FlipDemo/FlipDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlipDemo
+//
+//  Created by Wang on 16/6/1.
+//  Copyright © 2016年 云客. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
